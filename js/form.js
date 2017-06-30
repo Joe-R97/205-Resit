613fb8eb00132d744b01a618eb73ff6335e37133
Joe-R97 <robin241@uni.coventry.ac.uk>
2017-06-30 12:06:19 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,34 +1,25 @@
-// Get the modal
-var modal = document.getElementById('form');
+// Get's the element form from the document and assigns it to the variable 'form'
+var form = document.getElementById('form');
 
-// Get the button that opens the modal
-var btn = document.getElementById("showform");
+// Get the button that opens the form and assings it to the variable 'button'
+var button = document.getElementById("showform");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the <span> (close) element that closes the form
+var closes = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+// When the user clicks the button, opens the form 
+button.onclick = function() {
+    form.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+// When the user clicks on close (x), close the form
+closes.onclick = function() {
+    form.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the form, close it
 document.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-//Validates form making sure the corect information is given
-function validateForm() {
-    var x = document.forms["form"]["fullname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
+    if (event.target == form) {
+        form.style.display = "none";
     }
 }
